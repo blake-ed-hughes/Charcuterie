bef10b56648adf7cd91c8947cb618994c1aee14c
William Fink <wwfink30@gmail.com>
2021-09-10 16:11:30 -0500

    add specific modules to main app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
+import Overview from './ProductOverview.jsx';
+import Questions from './QnA.jsx';
+import Ratings from './RatingsnReviews.jsx';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state =({
+      productId: 38323
+    });
   }
 
   render(){
-    return (<p>hello world</p>);
+    return (
+    <main>
+      <Overview productId={this.state.productId}/>
+      <Questions productId={this.state.productId}/>
+      <Ratings productId={this.state.productId}/>
+    </main>
+      );
   }
 }
-
 
 export default App;
