1897d32b8d7380ba5bacb17ea3b7fc6eb04463c5
William Fink <wwfink30@gmail.com>
2021-09-13 18:40:56 -0500

    title and pirce + a few other house keeping things

--- src/components/ProductOverview.jsx
+++ src/components/ProductOverview.jsx
@@ -1,52 +1,68 @@
 import React from 'react';
 import {useState, useEffect} from 'react';
 import {getProduct, getProductStyles} from './poAxios';
+import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles((theme) => ({
+
+}));
 
 function Overview(props) {
-
-
-  const [style, setStyle] = useState(null);
-  const [styles, setStyles] = useState(null);
-  const [productInfo, setProductInfo] = useState(null);
+  const classes = useStyles();
+  const [pid, setPID] = useState(props.productId);
+  const [style, setStyle] = useState({});
+  const [styles, setStyles] = useState({});
+  const [productInfo, setProductInfo] = useState({});
   useEffect(() => {
-    getProductStyles(props.productId)
+    getProductStyles(pid)
     .then((response) => {
       setStyles(response.data.results);
       setStyle(response.data.results[0]);
     })
     .catch((err) => {console.log('styles', err)});
-    getProduct(props.productId)
+    getProduct(pid)
     .then((response) => {
       setProductInfo(response.data);
     })
     .catch((err) => {console.log('info', err)});
-  }, [])
+  }, [pid])
 
   return(
-    <Grid
-      container
-      direction="row"
-      justifyContent="space-around"
-      alignItems="flex-start"
-    >
-      <Grid xs container>
-        {/* this is my image gall */}
-        <p>{JSON.stringify(styles)}</p>
+    <div >
+      <Grid
+        container
+        direction="row"
+        justifyContent="space-around"
+        alignItems="flex-start"
+      >
+        <Grid item xs container>
+        <Paper >
+          {/* this is my image gall */}
+          <p>{JSON.stringify(styles)}</p>
+        </Paper>
+        </Grid>
+        <Grid item xs container direction="column">
+          <p>{JSON.stringify(productInfo)}</p>
+          <Paper>
+          {/* reviews */}
+          {/* category */}
+          {/* title */}
+          <Typography gutterBottom variant='h4'>{productInfo.name}</Typography>
+          {/* price */}
+          <Typography variant='subtitle1'>${productInfo.default_price}</Typography>
+          {/* style selector */}
+          {/*  */}
+          {/* add to cart */}
+          </Paper>
+        </Grid>
+        {/* possibly description */}
       </Grid>
-      <Grid xs container >
-        <p>{JSON.stringify(productInfo)}</p>
-        {/* reviews */}
-        {/* category */}
-        {/* title */}
-        {/* price */}
-        {/* style selector */}
-        {/*  */}
-        {/* add to cart */}
-      </Grid>
-      {/* possibly description */}
-    </Grid>
+    </div>
   )
 }
+
 
 export default Overview;
