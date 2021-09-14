88cb70e9b618306d360f03da37e174cc05691344
Pingping Xiao <ppx2021@gmail.com>
2021-09-14 16:24:03 -0500

    Added grid to Search, QuestionList, also added a feature of Yes button increment

--- src/components/qa/QnA.jsx
+++ src/components/qa/QnA.jsx
@@ -1,34 +1,53 @@
-import React from 'react';
+// resourses:
+import React, { useState } from 'react';
 import { Button } from '@material-ui/core';
 import Grid from '@material-ui/core/Grid';
-// import SearchBar from "material-ui-search-bar";
+import { makeStyles } from '@material-ui/core/styles';
+// components:
+import Paper from '@material-ui/core/Paper';
+import Yes from './Yes.jsx';
+import Search from './Search.jsx';
+import QuestionList from './QuestionList.jsx';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
 
+export default function QnA() {
+  const classes = useStyles();
 
-class QnA extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = ({
-      productId: 38322
-    });
-  }
-
-  render() {
-    return (
+  return (
+    <div className={classes.root}>
       <Grid container spacing={3}>
-        <div>
-          here will be a search bar
-        </div>
+
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>
+            <Search />
+          </Paper>
+        </Grid>
+
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>
+            <QuestionList />
+          </Paper>
+        </Grid>
+
+        <Grid item xs={3}>
+          <Paper className={classes.paper}>MORE ANSWERED QUESTIONS</Paper>
+        </Grid>
+
+        <Grid item xs={3}>
+          <Paper className={classes.paper}>ADD A QUESTION</Paper>
+        </Grid>
+
       </Grid>
-    );
-  }
+    </div>
+  );
 }
-
-export default QnA;
-
-
-//<SearchBar
-// value={this.state.value}
-// onChange={(newValue) => this.setState({ value: newValue })}
-// onRequestSearch={() => doSomethingWith(this.state.value)}
-// />
