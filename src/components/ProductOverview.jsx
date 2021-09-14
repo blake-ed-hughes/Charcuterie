ae6268755b9eb0a347226ec10a5887e1a32272c6
William Fink <wwfink30@gmail.com>
2021-09-13 21:11:48 -0500

    added temp image and description

--- src/components/ProductOverview.jsx
+++ src/components/ProductOverview.jsx
@@ -5,6 +5,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
+import Container from '@material-ui/core/Container';
 
 const useStyles = makeStyles((theme) => ({
 
@@ -16,11 +17,13 @@ function Overview(props) {
   const [style, setStyle] = useState({});
   const [styles, setStyles] = useState({});
   const [productInfo, setProductInfo] = useState({});
+  const [stylePhotos, setPhotos] = useState([{url:''}]);
   useEffect(() => {
     getProductStyles(pid)
     .then((response) => {
       setStyles(response.data.results);
       setStyle(response.data.results[0]);
+      setPhotos([...response.data.results[0].photos]);
     })
     .catch((err) => {console.log('styles', err)});
     getProduct(pid)
@@ -31,35 +34,42 @@ function Overview(props) {
   }, [pid])
 
   return(
-    <div >
-      <Grid
-        container
-        direction="row"
-        justifyContent="space-around"
-        alignItems="flex-start"
-      >
-        <Grid item xs container>
-        <Paper >
-          {/* this is my image gall */}
-          <p>{JSON.stringify(styles)}</p>
-        </Paper>
-        </Grid>
-        <Grid item xs container direction="column">
-          <p>{JSON.stringify(productInfo)}</p>
-          <Paper>
-          {/* reviews */}
-          {/* category */}
-          {/* title */}
-          <Typography gutterBottom variant='h4'>{productInfo.name}</Typography>
-          {/* price */}
-          <Typography variant='subtitle1'>${productInfo.default_price}</Typography>
-          {/* style selector */}
-          {/*  */}
-          {/* add to cart */}
+    <div>
+      <Container maxWidth="xl">
+        <Grid
+          container
+          spacing={3}
+          padding={3}
+          // direction="row"
+          // justifyContent="space-around"
+          // alignItems="flex-start"
+        >
+          <Grid item xs={12} sm={8} container justifyContent="center">
+          <Paper >
+            {/* this is my image gall */}
+            <img src={stylePhotos[stylePhotos.length-1].url} height='100%'/>
           </Paper>
+          </Grid>
+          <Grid item xs={12} sm={4} container direction="column">
+            <Paper >
+            {/* reviews */}
+            {/* category */}
+            <Typography variant='h6' color="textSecondary">{productInfo.category}</Typography>
+            {/* title */}
+            <Typography gutterBottom variant='h3'>{productInfo.name}</Typography>
+            {/* price */}
+            <Typography variant='subtitle1'>${productInfo.default_price}</Typography>
+            {/* style selector */}
+            {/*  */}
+            {/* add to cart */}
+            </Paper>
+          </Grid>
         </Grid>
         {/* possibly description */}
-      </Grid>
+        <Paper elevation={3}>
+          <Typography variant='subtitle2'>{productInfo.description}</Typography>
+        </Paper>
+      </Container>
     </div>
   )
 }
